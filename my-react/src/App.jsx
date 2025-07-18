5f0d96106512931d1a8bf83674a3872ce5dbb3f4
Chea Ritheavatey <2023447chea@aupp.edu.kh>
2025-07-18 11:23:57 +0700

    render list

--- my-react/src/App.jsx
+++ my-react/src/App.jsx
@@ -2,10 +2,11 @@ import Card from "./components/Card"
 import Button from "./components/Button"
 import Student from "./components/Student"
 import UserGreeting from "./components/UserGreeting"
+import List from "./components/List"
 function App() {
   return (
     <>
-
+      <List/>
       <Student name="spongebob" age={14} isStudent={true} />
       <hr />
       <Student name="patrick" age={34} isStudent={false} />
@@ -17,7 +18,7 @@ function App() {
       <Student />
       <hr />
       <UserGreeting isLoggedIn={true} username="my name"/>
-
+      <hr />
       <Button/>
       <br />
       <Card/>
---
+++ my-react/src/components/List.jsx
@@ -0,0 +1,35 @@
+function List() {
+    const fruits = [
+        {
+            id:1,
+            name: "apple",
+            calorie: 95
+        }, 
+        {
+            id:2,
+            name: "banana",
+            calorie: 105
+        }, 
+        {
+            id: 3,
+            name: "coconut",
+            calorie: 50
+        }, 
+        {
+            id: 4,
+            name: "durian",
+            calorie: 110}]
+    // fruits.sort((a,b) => a.name.localeCompare(b.name)); // a-> z
+    // fruits.sort((a,b) => b.name.localeCompare(a.name)); // z->a
+    
+    // sort calories
+    fruits.sort((a,b) => a.calorie-b.calorie);
+
+
+    const item = fruits.map(fruit => <li key={fruit.id}>{fruit.name}: {fruit.calorie}</li>)
+    
+    return (
+        <ul>{item}</ul>
+    )
+}
+export default List
